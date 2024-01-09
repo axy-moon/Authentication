bdd4de5384d640aefcccc22c7d702db42be899e4
Kiran Gutta <kirangutta@Kirans-MacBook-Air.local>
2024-01-09 16:58:16 +0530

    first commit

---
+++ login/Model/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  login
+//
+//  Created by Kiran Gutta on 09/01/24.
+//
+
+import Foundation
+
+struct UserModel {
+    var username : String
+    var email : String
+    var password : String
+}
---
+++ login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  login
+//
+//  Created by Kiran Gutta on 08/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailFIeld: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
